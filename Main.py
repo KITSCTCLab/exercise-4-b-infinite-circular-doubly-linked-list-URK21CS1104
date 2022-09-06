7e5e401e6534bbe94c46f504f06634a86c68de07
MADHAN L <110010905+URK21CS1104@users.noreply.github.com>
2022-09-06 22:26:37 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,11 +3,14 @@ length_of_circular_linked_list = int(input())
 # Read space-separated integers that denote the elements of the list which is returned as the output of the algorithm
 circular_linked_list = list(map(int,input().strip().split(" ")))
 
-new = []
-for i in example:
-    if i not in new:
-        new.append(i)
-
-print(len(new)
-for i in new:
+# 1 2 3 4 1 2 3 4 5 6
+new_list = []
+key = 0
+while len(new_list) < length_of_circular_linked_list and key < len(circular_linked_list):
+    element = circular_linked_list[key]
+    if element not in new_list:
+        new_list.append(element)
+    key += 1
+print(len(new_list))
+for i in new_list:
     print(i , end = " ")
